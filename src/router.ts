705b61f32d2ab4a93f92166425b7a00628b5e18e
JosdanCG <jdgalindo30@gmail.com>
2026-01-21 22:47:33 -0500

    21/01/26

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
+import User from "./models/User";
 
 const router = Router();
 
 router.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-router.post('/auth/register', (req, res) => {
+
+router.post('/auth/register', async (req, res) => {
     res.send('desde register');
   console.log('desde register');
   console.log(req.body);
   
-    
+  await User.create(req.body);
 });
 
 export default router;
