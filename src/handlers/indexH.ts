a0786fc096aed80bcb5963e583ba531cf7d885ef
JosdanCG <jdgalindo30@gmail.com>
2026-01-24 22:13:25 -0500

    24/01/26

--- src/handlers/indexH.ts
+++ src/handlers/indexH.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
 import User from "../models/User";
 
-export const createAcount = async(req: Request, res: Response) => {
+export const createAcount = async (req: Request, res: Response) => {
+    const { email } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        const error = new Error('El usuario ya está registrado');
+        return res.status(400).json({ msg: error.message });
+    }
     
     const user = new User(req.body);
     await user.save();
-    res.send('Registro creado correctamente');
+    res.status(201).send('Registro creado correctamente');
 }
