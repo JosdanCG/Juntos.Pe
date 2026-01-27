5c5b8a5547fe379247982e87f3693e9bb02f20aa
JosdanCG <jdgalindo30@gmail.com>
2026-01-26 22:23:06 -0500

    26/01/2026

--- src/handlers/indexH.ts
+++ src/handlers/indexH.ts
@@ -1,17 +1,35 @@
 import { Request, Response } from "express";
+import slug from 'slug'
 import User from "../models/User";
 import { hashPass } from "../utils/auth";
+import { validationResult } from "express-validator";
 
 export const createAcount = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password} = req.body;
     const userExists = await User.findOne({ email });
     if (userExists) {
-        const error = new Error('El usuario ya está registrado');
+        const error = new Error('El usuario con este email ya está registrado');
         return res.status(400).json({ msg: error.message });
+    }
+    return
+
+    const handle = slug(req.body.handle, '')
+    const handleExists = await User.findOne({ handle })
+    if (handleExists) { 
+        const error = new Error('Nombre de usuario no disponible')
+        return res.status(409).json({error: error.message})
     }
     
     const user = new User(req.body);
     user.password = await hashPass(password);
+    user.handle = handle
+
     await user.save();
     res.status(201).send('Registro creado correctamente');
 }
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { createAcount } from "./handlers/indexH";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.post('/auth/register', createAcount);
+router.post('/auth/register',
+    body('handle').notEmpty(),
+    createAcount);
 
 export default router;
